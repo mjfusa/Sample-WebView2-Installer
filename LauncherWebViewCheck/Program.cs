1224b4c954e7634fa13d1ef243b15a03a69587f8
Mike Francis <mike.francis@live.com>
2021-06-23 08:52:30 -0700

    initial push

---
+++ LauncherWebViewCheck/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Web.WebView2.Core;
+
+namespace LauncherWebViewCheck
+{
+    class Program
+    {
+        static public bool IsInstalled => WebView2Install.GetInfo().InstallType != InstallType.NotInstalled;
+        static public bool IsNotInstalled => !IsInstalled;
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello World! WebView2 is{(IsInstalled ? string.Empty : " not")} installed");
+
+            if (!IsInstalled)
+            {
+                using (Process myProcess = new Process())
+                {
+                    myProcess.StartInfo.UseShellExecute = false;
+                    myProcess.StartInfo.FileName = "MicrosoftEdgeWebView2RuntimeInstallerX64.exe";
+                    myProcess.StartInfo.CreateNoWindow = true;
+                    myProcess.Start();
+                }
+
+
+
+            }
+
+            Process.Start(new ProcessStartInfo("samplewebview2:") { UseShellExecute = true });
+            //using (Process myProcess = new Process())
+            //{
+            //    myProcess.StartInfo.UseShellExecute = false;
+            //    //myProcess.StartInfo.WorkingDirectory = "\\Sample-WebView2\\";
+            //    myProcess.StartInfo.FileName = "..\\Sample-WebView2\\Sample-WebView2.exe";
+            //    //myProcess.StartInfo.CreateNoWindow = true;
+            //    myProcess.Start();
+            //}
+
+
+        }
+    }
+}
